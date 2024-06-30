d78cd117d41781f9be9a47e10888b0c85b8fd747
XAzure 16 <21864847-XAzure16@users.noreply.gitlab.com>
2024-06-30 17:55:00 +0200

    add class Block

--- index.ts
+++ index.ts
@@ -1,13 +1,28 @@
-import * as crypto from 'crypto';
+import crypto from "node:crypto";
 
 class Transaction {
-constructor(
+  constructor(
     public amount: number,
     public payer: string, //public key
     public payee: string //public key
-){}
+  ) {}
 
-toString(){
+  toString() {
     return JSON.stringify(this);
+  }
 }
+
+class Block {
+  constructor(
+    public prevHash: string,
+    public transaction: Transaction,
+    public ts = Date.now()
+  ) {}
+
+  get hash() {
+    const str = JSON.stringify(this);
+    const hash = crypto.createHash("SHA256");
+    hash.update(str).end();
+    return hash.digest("hex");
+  }
 }
